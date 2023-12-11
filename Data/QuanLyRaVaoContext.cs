b82ab39d59616a3a33f3c6dccf8cff1b1c206cd5
hongquan_pt <nguyenhongquan122002@gmail.com>
2023-12-11 07:28:52 +0700

    ...

--- Data/QuanLyRaVaoContext.cs
+++ Data/QuanLyRaVaoContext.cs
@@ -45,7 +45,6 @@ public partial class QuanLyRaVaoContext : DbContext
     public virtual DbSet<Vipham> Viphams { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=Quan\\hq;Initial Catalog=QuanLyRaVao;Integrated Security=True;Encrypt=false;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
